7ddde5840eefb8aef0b16f8768e16286174043be
Kunal1454 <152690151+Kunal1454@users.noreply.github.com>
2024-01-10 09:53:09 +0000

    added

--- my-main/src/App.js
+++ my-main/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import home from './components/home';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Kunal and lamber website
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <home/>
     </div>
+    </>
   );
 }
 
---
+++ my-main/src/components/home.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from '../logo.svg';
+
+export default function home() {
+  return (
+    <div>
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Kunal and Nishant website
+        </p>
+        <h1>
+            This is home page
+        </h1>
+      </header> 
+    </div>
+  )
+}
