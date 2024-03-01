9caf25a230a6baa1a09855baaeaba86a8fdc47b8
kunal&nishant <152690151+Kunal1454@users.noreply.github.com>
2024-03-01 05:36:14 +0000

    commit 3

--- my-main/src/components/home.js
+++ my-main/src/components/home.js
@@ -1,18 +1,11 @@
-import React from 'react'
-import logo from '../logo.svg';
+import React from "react";
+import logo from "../logo.svg";
 
 export default function home() {
   return (
-    <div>
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Kunal and Nishant website
-        </p>
-        <h1>
-            This is home page
-        </h1>
-      </header> 
-    </div>
-  )
+    <header className="App-header">
+      {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <h1>Our Website</h1>
+    </header>
+  );
 }
