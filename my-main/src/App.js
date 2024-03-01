bb1d74b67c77953fb59fd2b9385a6a345d071674
kunal&nishant <152690151+Kunal1454@users.noreply.github.com>
2024-03-01 05:12:29 +0000

    initial commit on sandbox

--- my-main/src/App.js
+++ my-main/src/App.js
@@ -1,13 +1,20 @@
-import './App.css';
-import home from './components/home';
+import "./App.css";
+import Home from "./components/home";
+import { motion } from "framer-motion";
+import Header from "./components/header";
 
 function App() {
   return (
-    <>
-    <div className="App">
-      <home/>
-    </div>
-    </>
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5, delay: 0.1 }}
+    >
+      <div className="App">
+        <Header />
+        <Home />
+      </div>
+    </motion.div>
   );
 }
 
--- my-main/src/components/home.js
+++ my-main/src/components/home.js
@@ -1,18 +1,12 @@
-import React from 'react'
-import logo from '../logo.svg';
+import React from "react";
+import logo from "../logo.svg";
 
 export default function home() {
   return (
-    <div>
-        <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Kunal and Nishant website
-        </p>
-        <h1>
-            This is home page
-        </h1>
-      </header> 
-    </div>
-  )
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>Kunal and Nishant website</p>
+      <h1>This is home page</h1>
+    </header>
+  );
 }
